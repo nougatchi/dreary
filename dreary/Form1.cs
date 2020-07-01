3d5c45c2fd97adeab7ad0d718f7b832af25987a2
ryelow90210 <disposablegrain.old@protonmail.com>
2020-07-01 13:37:33 -0400

    add comments

--- dreary/Form1.cs
+++ dreary/Form1.cs
@@ -13,10 +13,10 @@ namespace dreary
 {
     public partial class Form1 : Form
     {
-        public Scene scene;
-        public ActionList actionlist;
-        public Camera pcam;
-        public DateTime time;
+        public Scene scene; // the game scene
+        public ActionList actionlist; // list of actions opengl has to do (acting funny for some reason)
+        public Camera pcam; // player camera
+        public DateTime time; // time since program start (gameinit being called and creating all the assets)
         public Form1()
         {
             InitializeComponent();
@@ -26,47 +26,48 @@ namespace dreary
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            InitGame();
+            InitGame(); // initgame creates pcam, actionlist, scene and time
         }
 
         private void Match(TreeView treeView, SceneNodeBase nodeBase)
         {
-            treeView.Nodes.Clear();
-            var node = new TreeNode(nodeBase.Name) { Tag = nodeBase };
+            treeView.Nodes.Clear(); // clear everything in the treeview
+            var node = new TreeNode(nodeBase.Name) { Tag = nodeBase }; // add a node, set its name to the Name field of nodebase
             treeView.Nodes.Add(node);
-            node.ImageIndex = DeviconServe.GetDeviconIndex(nodeBase.GetType().Name);
+            node.ImageIndex = DeviconServe.GetDeviconIndex(nodeBase.GetType().Name); // get the devicon of this node
             node.SelectedImageIndex = DeviconServe.GetDeviconIndex(nodeBase.GetType().Name);
-            var anode = new TreeNode(actionlist.GetType().Name) { Tag = actionlist };
+            var anode = new TreeNode(actionlist.GetType().Name) { Tag = actionlist }; // anode is just the action list
             treeView.Nodes.Add(anode);
-            anode.ImageIndex = DeviconServe.GetDeviconIndex(actionlist.GetType().Name);
+            anode.ImageIndex = DeviconServe.GetDeviconIndex(actionlist.GetType().Name); // get the devicon of anode
             anode.SelectedImageIndex = DeviconServe.GetDeviconIndex(actionlist.GetType().Name);
-            Match(node, nodeBase);
-            Match(anode, actionlist);
+            Match(node, nodeBase); // call for each child
+            Match(anode, actionlist); // call for each child
         }
 
         private void Match(TreeNode node, SceneNodeBase nodeBase)
         {
-            foreach (var item in nodeBase.Children)
+            foreach (var item in nodeBase.Children) // item is based off of SceneNodeBase
             {
-                var child = new TreeNode(item.Name) { Tag = item };
+                var child = new TreeNode(item.Name) { Tag = item }; // add a node, set its name to the Name field of item
                 node.Nodes.Add(child);
                 child.ImageIndex = DeviconServe.GetDeviconIndex(item.GetType().Name);
                 child.SelectedImageIndex = DeviconServe.GetDeviconIndex(item.GetType().Name);
                 Match(child, item);
             }
         }
-        private void Match(TreeNode node, ActionList nodeBase)
+        private void Match(TreeNode node, ActionList nodeBase) 
         {
-            foreach (var item in nodeBase)
+            foreach (var item in nodeBase) // item is a task that is put in ActionList
             {
-                var child = new TreeNode(item.GetType().Name) { Tag = item };
+                var child = new TreeNode(item.GetType().Name) { Tag = item }; // add a node, set its name to the type of its class
                 child.ImageIndex = DeviconServe.GetDeviconIndex(item.GetType().Name);
                 child.SelectedImageIndex = DeviconServe.GetDeviconIndex(item.GetType().Name);
                 node.Nodes.Add(child);
+                // dont call Match at all because the tasks itself dont contain children
             }
         }
 
-        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e) // draw code
         {
             ActionList list = actionlist;
             if (list != null)
@@ -81,7 +82,7 @@ namespace dreary
                     tasks += $"{i.GetType().Name} ";
                 }
                 GL.Instance.DrawText(10, 34, Color.Red, "Arial", 12, tasks);
-                list.Act(new ActionParams(Viewport.GetCurrent()));
+                list.Act(new ActionParams(Viewport.GetCurrent())); // this apparently wont work. gl is known to work as it draws above and below but this wont budge at all
                 GL.Instance.DrawText(10, 10, Color.Red, "Arial", 12, "FPS: " + winGLCanvas1.FPS.ToString() + " Time " + (DateTime.Now - time).Seconds);
             }
         }
@@ -91,15 +92,18 @@ namespace dreary
             propertyGrid1.SelectedObject = e.Node.Tag;
         }
 
+        /// <summary>
+        /// InitGame initialises all of the classes (pcam, scene, time, the game scene root). It must be called.
+        /// </summary>
         private void InitGame()
         {
             var position = new vec3(5, 3, 4);
             var center = new vec3(0, 0, 0);
             var up = new vec3(0, 1, 0);
-            pcam = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
-            GroupNode rootElement = new GroupNode();
+            pcam = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height); // create the camera
+            GroupNode rootElement = new GroupNode(); // this will be the rootnode of the scene
             CreateGameTree(rootElement);
-            scene = new Scene(pcam)
+            scene = new Scene(pcam) // initialises the scene, use pcam as camera
             {
                 RootNode = rootElement,
                 ClearColor = Color.Black.ToVec4(),
@@ -108,48 +112,52 @@ namespace dreary
             treeView1.ExpandAll();
             time = new DateTime();
 
-            var list = new ActionList();
-            var transformAction = new TransformAction(scene);
+            var list = new ActionList(); // iniitialise the actionlist
+            var transformAction = new TransformAction(scene); // transform action
             list.Add(transformAction);
-            var billboardSortAction = new BillboardSortAction(scene.RootNode, scene.Camera);
+            var billboardSortAction = new BillboardSortAction(scene.RootNode, scene.Camera); // billboard sort
             list.Add(billboardSortAction);
-            var renderAction = new RenderAction(scene);
+            var renderAction = new RenderAction(scene); // render
             list.Add(renderAction);
-            var billboardRenderAction = new BillboardRenderAction(scene.Camera, billboardSortAction);
+            var billboardRenderAction = new BillboardRenderAction(scene.Camera, billboardSortAction); // billboard render
             list.Add(billboardRenderAction);
             actionlist = list;
-            var manipulater = new FirstPerspectiveManipulater();
+            var manipulater = new FirstPerspectiveManipulater(); // allows moving the camera
             manipulater.BindingMouseButtons = GLMouseButtons.Right;
             manipulater.StepLength = 0.1f;
             manipulater.Bind(pcam, winGLCanvas1);
-            Match(treeView1, scene.RootNode);
+            Match(treeView1, scene.RootNode); // update the treeview
         }
 
+        /// <summary>
+        /// This places all of the smaller classes in the root node
+        /// </summary>
+        /// <param name="node">The root node in question</param>
         private void CreateGameTree(GroupNode node)
         {
-            node.Initialize();
-            CameraNode cameraNode = new CameraNode(pcam);
+            node.Initialize(); // initialise root node
+            CameraNode cameraNode = new CameraNode(pcam); // bind cameranode to pcam
             cameraNode.Name = "Camera";
-            node.Children.Add(cameraNode);
-            cameraNode.Initialize();
-            TextBillboardNode billboardNode = TextBillboardNode.Create(200, 40, 100);
+            node.Children.Add(cameraNode); // add this to the rootnode
+            cameraNode.Initialize(); // init camnode
+            TextBillboardNode billboardNode = TextBillboardNode.Create(200, 40, 100); // create a billboard
             billboardNode.EnableRendering = ThreeFlags.None;
             billboardNode.Color = Color.White.ToVec3();
-            billboardNode.Text = "Hello";
+            billboardNode.Text = "Hello"; // sets the billboard text to Hello
             billboardNode.WorldPosition = new vec3(5, 0, 0);
-            billboardNode.Name = "Billboard_Dbg";
-            node.Children.Add(billboardNode);
-            Nodes.SkyboxNode skybox = Nodes.SkyboxNode.Create(new Bitmap(800, 800, System.Drawing.Imaging.PixelFormat.Format8bppIndexed));
+            billboardNode.Name = "Billboard_Dbg"; // set the billboard name to Billboard_Dbg
+            node.Children.Add(billboardNode); // add this to the rootnode
+            Nodes.SkyboxNode skybox = Nodes.SkyboxNode.Create(new Bitmap(800, 800, System.Drawing.Imaging.PixelFormat.Format8bppIndexed)); // create the skybox
             try
             {
-                skybox = Nodes.SkyboxNode.Create(new Bitmap(Image.FromFile("Content/e30cwk97.bmp")));
+                skybox = Nodes.SkyboxNode.Create(new Bitmap(Image.FromFile("Content/e30cwk97.bmp"))); // attempt loading the skybox "e30cwk97.bmp"
             }
             catch (Exception e)
             {
 
             }
             skybox.Name = "Skybox";
-            node.Children.Add(skybox);
+            node.Children.Add(skybox); // add this to the rootnode
         }
     }
 }
