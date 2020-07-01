237e8e732a46b19cf6d80c1847c14ae5c7d1610a
ryelow90210 <disposablegrain.old@protonmail.com>
2020-07-01 12:06:44 -0400

    Add project files.

---
+++ dreary/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace dreary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.winGLCanvas1 = new CSharpGL.WinGLCanvas();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // winGLCanvas1
+            // 
+            this.winGLCanvas1.AccumAlphaBits = ((byte)(0));
+            this.winGLCanvas1.AccumBits = ((byte)(0));
+            this.winGLCanvas1.AccumBlueBits = ((byte)(0));
+            this.winGLCanvas1.AccumGreenBits = ((byte)(0));
+            this.winGLCanvas1.AccumRedBits = ((byte)(0));
+            this.winGLCanvas1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.winGLCanvas1.Location = new System.Drawing.Point(0, 0);
+            this.winGLCanvas1.Name = "winGLCanvas1";
+            this.winGLCanvas1.RenderTrigger = CSharpGL.RenderTrigger.TimerBased;
+            this.winGLCanvas1.Size = new System.Drawing.Size(938, 606);
+            this.winGLCanvas1.StencilBits = ((byte)(0));
+            this.winGLCanvas1.TabIndex = 0;
+            this.winGLCanvas1.TimerTriggerInterval = 40;
+            this.winGLCanvas1.UpdateContextVersion = true;
+            this.winGLCanvas1.OpenGLDraw += new CSharpGL.GLEventHandler<System.Windows.Forms.PaintEventArgs>(this.winGLCanvas1_OpenGLDraw);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.propertyGrid1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.winGLCanvas1);
+            this.splitContainer1.Size = new System.Drawing.Size(1410, 606);
+            this.splitContainer1.SplitterDistance = 468;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.propertyGrid1.Location = new System.Drawing.Point(0, 333);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(468, 273);
+            this.propertyGrid1.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(468, 327);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 606);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Dreary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.winGLCanvas1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CSharpGL.WinGLCanvas winGLCanvas1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ dreary/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpGL;
+
+namespace dreary
+{
+    public partial class Form1 : Form
+    {
+        public Scene scene;
+        public ActionList actionlist;
+        public Camera pcam;
+        public DateTime time;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            var position = new vec3(5, 3, 4);
+            var center = new vec3(0, 0, 0);
+            var up = new vec3(0, 1, 0);
+            pcam = new Camera(position, center, up, CameraType.Perspective, this.winGLCanvas1.Width, this.winGLCanvas1.Height);
+            GroupNode rootElement = new GroupNode();
+            {
+                CameraNode cameraNode = new CameraNode(pcam);
+                rootElement.Children.Add(cameraNode);
+                cameraNode.Initialize();
+                TextBillboardNode billboardNode = TextBillboardNode.Create(200, 40, 100);
+                billboardNode.EnableRendering = ThreeFlags.None;
+                billboardNode.Color = Color.White.ToVec3();
+                billboardNode.Text = "Hello";
+                billboardNode.WorldPosition = new vec3(5, 0, 0);
+                rootElement.Children.Add(billboardNode);
+                Nodes.SkyboxNode skybox = Nodes.SkyboxNode.Create(new Bitmap(800, 800, System.Drawing.Imaging.PixelFormat.Format8bppIndexed));
+                try
+                {
+                    skybox = Nodes.SkyboxNode.Create(new Bitmap(Image.FromFile("Content/e30cwk97.bmp")));
+                } catch(Exception e) { 
+
+                }
+                rootElement.Children.Add(skybox);
+            }
+            scene = new Scene(pcam)
+            {
+                RootNode = rootElement,
+                ClearColor = Color.Black.ToVec4(),
+            };
+            actionlist = new ActionList();
+            Match(treeView1, scene.RootNode);
+            treeView1.ExpandAll();
+            time = new DateTime();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var list = new ActionList();
+            var transformAction = new TransformAction(scene);
+            list.Add(transformAction);
+            var billboardSortAction = new BillboardSortAction(scene.RootNode, scene.Camera);
+            var renderAction = new RenderAction(scene);
+            list.Add(renderAction);
+            var billboardRenderAction = new BillboardRenderAction(scene.Camera, billboardSortAction);
+            list.Add(billboardRenderAction);
+            actionlist = list;
+            var manipulater = new FirstPerspectiveManipulater();
+            manipulater.Bind(pcam, winGLCanvas1);
+        }
+
+        private void Match(TreeView treeView, SceneNodeBase nodeBase)
+        {
+            treeView.Nodes.Clear();
+            var node = new TreeNode(nodeBase.ToString()) { Tag = nodeBase };
+            treeView.Nodes.Add(node);
+            Match(node, nodeBase);
+        }
+
+        private void Match(TreeNode node, SceneNodeBase nodeBase)
+        {
+            foreach (var item in nodeBase.Children)
+            {
+                var child = new TreeNode(item.ToString()) { Tag = item };
+                node.Nodes.Add(child);
+                Match(child, item);
+            }
+        }
+
+        private void winGLCanvas1_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            ActionList list = actionlist;
+            if (list != null)
+            {
+                vec4 clearColor = scene.ClearColor;
+                GL.Instance.ClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+                GL.Instance.Clear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT | GL.GL_STENCIL_BUFFER_BIT);
+                list.Act(new ActionParams(Viewport.GetCurrent()));
+                GL.Instance.DrawText(10, 10, Color.Red, "Arial", 12, "FPS: " + winGLCanvas1.FPS.ToString() + " Time " + (DateTime.Now - time).Seconds);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            propertyGrid1.SelectedObject = e.Node.Tag;
+        }
+    }
+}
